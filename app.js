5a311db848afa5e7b26783eeded427d2b68ac76f
meytizn <zamanianmehdi23desktop@gmail.com>
2026-08-13 16:53:30 +0330

    installing pug template engine and render homepage

--- app.js
+++ app.js
@@ -5,16 +5,21 @@ const app = express()
 const port = 3000 ;
 
 
+// template engin pug
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+
 // routes
+
 app.use("/" ,homeRouter )
 app.use("/users" ,userRouter)
 
 
 
+// configuration
+
 app.listen( port , ()=>{ 
     console.log(`app is runnig on port ${port}`) 
 } )
-
-
-
-// nodemon app.js   to run app
